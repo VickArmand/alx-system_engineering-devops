c35ef38c54eb80ce383884d30680f04bdec71ad6
VickArmand <victormaina1962@gmail.com>
2023-12-22 11:18:06 +0300

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,8 @@
+# create a file in /tmp
+
 file{ '/tmp/school':
     ensure  => 'present',
+    path    => '/tmp/school',
     content => 'I love Puppet',
     mode    => '0744',
     owner   => 'www-data',
