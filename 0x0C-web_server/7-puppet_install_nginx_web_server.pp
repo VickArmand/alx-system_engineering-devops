b07d87cff460c0ff4c109a38ae06c2edffd76ebb
VickArmand <victormaina1962@gmail.com>
2024-01-11 21:24:09 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,3 +8,32 @@
 #	Your answer file should be a Puppet manifest containing commands
 #	to automatically configure an Ubuntu machine to
 #	respect above requirements
+exec {
+  command  => 'sudo apt-get update -y'
+  provider => 'shell'
+}
+
+exec {
+  command => 'sudo apt-get install nginx -y'
+  provider => 'shell'
+}
+
+file {'/usr/share/nginx/html/index.html'
+  content => 'Hello World!'
+}
+
+file {'/var/www/html/custom_404.html'
+  content => "Ceci n'est pas une page"
+}
+
+exec {
+  command => 'sudo sed -i "30i \\\tlocation /redirect_me {\n\t\treturn 301 https://intranet.alxswe.com;\n\t}\n" /etc/nginx/sites-available/default'
+  provider => 'shell'
+}
+
+exec {
+  command => 'sudo service nginx restart'
+  provider => 'shell'
+}
+
+
