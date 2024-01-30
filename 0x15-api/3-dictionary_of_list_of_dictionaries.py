35cdc3cb0a068335fc9e7fb830e7df01aa216b2b
VickArmand <victormaina1962@gmail.com>
2024-01-30 14:29:49 +0300

    corrections v2

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+extend your Python script to export data in the JSON format.
+
+Requirements:
+    Records all tasks from all employees
+    Format must be: { "USER_ID": [ {"username": "USERNAME",
+    "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
+    {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS}, ... ],
+    "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS},
+    {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS}, ... ]}
+    File name must be: todo_all_employees.json
+"""
