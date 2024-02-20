9cf35f698208d177ac8cfabae20fe66e4980a182
VickArmand <victormaina1962@gmail.com>
2024-02-20 13:52:54 +0300

    corrections v1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,16 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     If an invalid subreddit is given, the function should return 0.
+    Hint: No authentication is necessary for most features of the Reddit API.
+    If you’re getting errors related to Too Many Requests,
+    ensure you’re setting a custom User-Agent.
+    NOTE: Invalid subreddits may return a redirect to search results.
+    Ensure that you are not following redirects.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    subscribers = response.json().get('data').get('subscribers')
-    return subscribers
+    results = response.json().get("data")
+    return results.get("subscribers")
